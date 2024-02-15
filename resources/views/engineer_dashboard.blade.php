f3da7a6c76738f0535fd2eacdd616a7be79dcd28
Mani-Amanda <135225894+Mani-Amanda@users.noreply.github.com>
2024-02-15 22:29:46 +0530

    change aircarft list in engineer dash board

--- resources/views/engineer_dashboard.blade.php
+++ resources/views/engineer_dashboard.blade.php
@@ -1,4 +1,11 @@
 <x-app-layout>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    </head>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Engineer Dashboard') }}
@@ -8,29 +15,39 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                {{-- Iterate over aircrafts and display their details --}}
-                    <h3>Aircrafts List</h3>
+            <div class="row justify-content-center mb-4">
+        <div class="col-auto">
+            <h3>Aircrafts List</h3>
+        </div>
+    </div>
+    <div class="row">
+    
+        @php
+            $displayedCombinations = [];
+        @endphp
+
+        @foreach ($aircrafts as $aircraft)
+            @php
+                $combination = $aircraft->type . '-' . $aircraft->frameno;
+            @endphp
+
+            @if (!in_array($combination, $displayedCombinations))
+                <div class="col-md-4">
+                <div class="text-center">
                     <ul>
-                        @php
-                            $displayedCombinations = [];
-                            @endphp
-
-                            @foreach ($aircrafts as $aircraft)
-                            @php
-                                $combination = $aircraft->type . '-' . $aircraft->frameno;
-                            @endphp
-
-                        @if (!in_array($combination, $displayedCombinations))
-                            <li>{{ $aircraft->type }} - {{ $aircraft->frameno }}</li>
-                            {{-- Display other aircraft details as needed --}}
-                            @php
-                                $displayedCombinations[] = $combination;
-                            @endphp
-                        @endif
-                    @endforeach
-                </ul>
-            </div>
+                        <li>{{ $aircraft->type }} - {{ $aircraft->frameno }}</li>
+                        {{-- Display other aircraft details as needed --}}
+                    </ul>
+                    </div>
+                </div>
+                @php
+                    $displayedCombinations[] = $combination;
+                @endphp
+            @endif
+        @endforeach
+    
+    </div>
+</div>
         </div>
         </div>
     </div>
--- resources/views/pilot_dashboard.blade.php
+++ resources/views/pilot_dashboard.blade.php
@@ -4,7 +4,6 @@
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Two Column Layout</title>
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
         <x-slot name="header">
